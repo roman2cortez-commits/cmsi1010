1becce76c4e03242a875e5cb2881bb259371d5d1
Roman Cortez <roman2cortez@gmail.com>
2026-09-16 16:45:27 -0700

    First three labs

---
+++ lab01/triangle.py
@@ -0,0 +1,7 @@
+def triangle(character,lines):
+    for count in range(1,lines+1):
+        print(count*character)
+
+triangle(character="*",lines=2)
+triangle(character="*",lines=8)
+triangle(character="*",lines=8)
