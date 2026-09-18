051879d6fcc0d3580bbdbe38da9620d5e8c9317b
Roman Cortez <roman2cortez@gmail.com>
2026-09-18 15:33:27 -0700

    pizza loops completed

--- multiple List/loop/loop.py
+++ multiple List/loop/loop.py
@@ -1,2 +1,12 @@
-avalible_options = ['mushrooms','olives','green peppers', 'cheese','garlic']
+avalible_toppings = ['mushrooms','olives','green peppers', 'extra cheese','garlic']
+
+requested_toppings = ['mushrooms','french fries','extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in avalible_toppings:
+        print(f"adding {requested_topping}.")
+    else:
+        print(f"sorry we dont have the{requested_topping}.")
+
+print("\nFinished making your pizza!")
 
