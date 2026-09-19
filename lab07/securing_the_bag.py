28b00059fe31c562b5fb1d8ec029fabec5b1c959
Roman Cortez <roman2cortez@gmail.com>
2026-09-19 01:21:08 -0700

    including taxes

--- lab07/securing_the_bag.py
+++ lab07/securing_the_bag.py
@@ -1,9 +1,14 @@
-balance=1000
-interest_rate=.07
-years=20 
+balance = 10000
+interest_rate = 0.14
+tax_rate = 0.25         
+years = 40
+deposit = 1000
 
-print("At the start you have", balance)
+print(f"at the start you have,{balance:.2f}")
 for year in range(1, years + 1):
     interest_earned = balance * interest_rate
+    taxes = interest_earned * tax_rate
+    balance += taxes
+    balance += deposit
     balance += interest_earned
-    print("after year",year,"you have",balance)
+    print(f"After year {year} you have {balance:.2f}")
