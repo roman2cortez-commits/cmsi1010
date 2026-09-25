f953f74e0b78915837080e01b455fd107af6e799
Roman Cortez <roman2cortez@gmail.com>
2026-09-25 16:01:47 -0700

    opened number guesser

---
+++ Pset01/number_guesser.py
@@ -0,0 +1,34 @@
+# ----------------------------------------------------------------------
+# This is the file number_guesser.py
+#
+# The intent is to give you practice writing a complete, interactive
+# Python program.
+#
+# Remove ALL of the existing comments in this file prior to submission.
+# You can, and should, add your own comments, but please remove all the
+# comments that are here now.
+#
+# Things to do:
+#git 
+# Generate a random number between 1 and 1000.
+#
+# Ask the user to guess the number. In your prompt, let the user know they
+# can type 'bye' or 'exit' to quit the program.
+#
+# If their guess is not made up entirely of digits, print "Please enter a valid
+# number" and ask them to guess again.
+#
+# If the guess is too high, print "Too high!" and continue asking.
+#
+# If the guess is too low, print "Too low!" and continue asking.
+#
+# If the guess is correct, print "Congratulations! You guessed the number!" along
+# with the number of attempts it took to guess the number. Start over with a new
+# random number. Make sure to zero out the number of attempts.
+#
+# Please note: There are likely to be a number of Python guessing games online,
+# and most GenAI systems can probably write this for you. Don’t rely on them,
+# as they rob you of a chance to practice your Python skills and they might not
+# even be correct. Perhaps, worse, they might not follow the instructions
+# exactly as given.
+# ----------------------------------------------------------------------
